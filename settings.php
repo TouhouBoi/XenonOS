76188d8dc0ae8ea87e7b67a8e45fc0c6a546d1e1
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-11-14 16:17:35 -0500

    Restructure settings.php

--- settings.php
+++ settings.php
@@ -1,26 +1,30 @@
 <?php
 /*
-  ___  _ __   ___ _   _  ___
- / _ \| '_ \ / _ \ | | |/ _ \
-| (_) | | | |  __/ |_| |  __/
- \___/|_| |_|\___|\__, |\___|
-                  |___/
+##     ## ######## ##    ##  #######  ##    ##  #######   ######
+ ##   ##  ##       ###   ## ##     ## ###   ## ##     ## ##    ##
+  ## ##   ##       ####  ## ##     ## ####  ## ##     ## ##
+   ###    ######   ## ## ## ##     ## ## ## ## ##     ##  ######
+  ## ##   ##       ##  #### ##     ## ##  #### ##     ##       ##
+ ##   ##  ##       ##   ### ##     ## ##   ### ##     ## ##    ##
+##     ## ######## ##    ##  #######  ##    ##  #######   ######
 
-oneye is released under the GNU Affero General Public License Version 3 (AGPL3)
- -> provided with this release in license.txt
- -> or via web at www.gnu.org/licenses/agpl-3.0.txt
 
+XenonOS is a fork of the oneye project.
+XenonOS Copyright © 2019 Trinity (touhouboi@protonmail.com)
+
+Original Copyright
+------------------------------------------
+https://oneye-project.org
 Copyright © 2005 - 2010 eyeos Team (team@eyeos.org)
              since 2010 Lars Knickrehm (mail@lars-sh.de)
+
 */
 
 /*
-	Main configuration file.
-	Please read the oneye configuration documentation
-	for more information before modifying anything here.
+	XenonOS Main Configuration file.
 */
 
-//Paths
+// File Paths
 define('EYE_ROOT','.');
 define('REAL_EYE_ROOT','system');
 define('SYSTEM_DIR','system');
@@ -45,38 +49,44 @@ define('GROUPS_DIR','groups');
 define('FILES_GROUP_DIR','files');
 define('CONF_GROUP_DIR','conf');
 define('LOG_DIR','logs');
-//oneye file extensions
+define('EYEOS_TMP_DIR','tmp');
+
+// XenonOS file extensions
 define('EYEOS_INFO_EXT','eyeInfo');
 define('EYEOS_FILE_EXT','eyeFile');
 define('EYEOS_LINK_EXT','eyeLink');
 define('EYE_CODE_EXTENSION','.eyecode');
 define('EYEOS_TRASH_EXT','eyeTrash');
 
-//vfs module to use
+// vfs module to use
 define('VFS_MODULE','virtual');
 
-//um module to use
+// um module to use
 define('UM_MODULE','oneye');
 
-//The real god of oneye users, master of masters
+// define root account
 define('REAL_ROOTUSER','root');
 
-//Extras
-define('EYEOS_TMP_DIR','tmp');
+// XenonOS Version
 define('EYEOS_VERSION','1.0.0alpha');
 define('ONEYE_VERSION','1.0.0alpha');
-define('XML_COMPAT',1);
-define('XML_PARSER','pure');
-define('ACL_SUPPORT',1);
-define('TIMEZONE','UTC'); // It must be a valid timezone identifier: http://www.php.net/manual/en/timezones.php
-//eyeDialog that defines should be moved to another place desinged for it.
+define('KE_KERNEL_VERSION','1.0.0alpha');
+
+// Define XenonOS Dialog Types
 define('EYEDIALOG_TYPE_OPENFILE', 0);
 define('EYEDIALOG_TYPE_SAVEFILE', 1);
 define('EYEDIALOG_TYPE_SELECTFOLDER', 2);
 
-define('CHECK_MOBILE',1);
+// DEPRECATED
+define('CHECK_MOBILE', 0);
 
 // User Management: LDAP
 define('LDAP_DN', 'uid=%0,ou=People,o=localhost');
 define('LDAP_SERVER', 'localhost');
+
+// Extras
+define('XML_COMPAT',1);
+define('XML_PARSER','pure');
+define('ACL_SUPPORT',1);
+define('TIMEZONE','UTC'); // It must be a valid timezone identifier: http://www.php.net/manual/en/timezones.php
 ?>
